2a743ae10254e29d0ad4c984a664c75ef822c116
JohnDoeTheFan <JohnDoeTheFan@gmail.com>
2021-09-25 23:55:57 +0900

    AirCombo added.

--- Assets/Scripts/Abilities/MeleeAttackSystem/MeleeAttacker.cs
+++ Assets/Scripts/Abilities/MeleeAttackSystem/MeleeAttacker.cs
@@ -8,7 +8,9 @@ public class MeleeAttacker : AbilityBase, MeleeAttack.ISubscriber
 {
     [SerializeField] private string skill1Name;
     [SerializeField] private Sprite skill1Sprite;
-    [SerializeField] List<ComboInformation> combos;
+    [SerializeField] List<ComboInformation> groundCombos;
+    [SerializeField] List<ComboInformation> airCombos;
+
     [Header("Rewards")]
     [SerializeField] GameObject reward;
     [SerializeField] float rewardProbability;
@@ -21,14 +23,19 @@ public class MeleeAttacker : AbilityBase, MeleeAttack.ISubscriber
     private Coroutine comboResetTimer;
 
     private bool isOccupiedByThis;
-    private bool isInitializing;
+    private bool isResetting;
+
+    private List<ComboInformation> currentCombos;
 
     protected override void Start()
     {
         skills.Add(new ButtonActiveAbilitySKill(skill1Name, skill1Sprite, 0f, new SkillDescription(), StartCoroutine, OnSkill1, IsAvailable));
         HashSet<MeleeAttack> processedSet = new HashSet<MeleeAttack>();
 
-        foreach (ComboInformation combo in combos)
+        List<ComboInformation> allCombos = new List<ComboInformation>(groundCombos);
+        allCombos.AddRange(airCombos);
+
+        foreach (ComboInformation combo in allCombos)
         {
             foreach (DataOnTime<MeleeAttack> attackOnTime in combo.attackOnTime)
             {
@@ -47,15 +54,25 @@ public class MeleeAttacker : AbilityBase, MeleeAttack.ISubscriber
 
     public bool IsAvailable()
     {
-        if (isInitializing)
+        if (isResetting)
             return false;
         if (abilityHolder.isMovementOccupied && !isOccupiedByThis)
             return false;
 
-        if (comboCount + 1 < combos.Count)
+        if(currentCombos != null)
         {
-            if (!combos[comboCount + 1].canUseInAir && !abilityHolder.IsGrounded)
-                return false;
+            if (comboCount + 1 < currentCombos.Count)
+            {
+                if (!currentCombos[comboCount + 1].canUseInAir && !abilityHolder.IsGrounded)
+                    return false;
+            }
+        }
+        else
+        {
+            if (abilityHolder.IsGrounded)
+                return groundCombos.Count > 0 ? true : false;
+            else
+                return airCombos.Count > 0 ? true : false;
         }
 
         return true;
@@ -70,11 +87,15 @@ public class MeleeAttacker : AbilityBase, MeleeAttack.ISubscriber
         bool isFirstCombo = comboCount == -1;
         if (isFirstCombo)
         {
+            if (abilityHolder.IsGrounded)
+                currentCombos = groundCombos;
+            else
+                currentCombos = airCombos;
             StartNextCombo();
         }
         else
         {
-            ComboInformation currentCombo = combos[comboCount];
+            ComboInformation currentCombo = currentCombos[comboCount];
             float nextComboInputStartTime = currentComboStartTime + currentCombo.nextComboInputStart;
             float nextComboInputEndTime = nextComboInputStartTime + currentCombo.nextComboInputDuration;
             bool canStartNextCombo = IsInTimeRange(Time.time, nextComboInputStartTime, nextComboInputEndTime);
@@ -93,13 +114,13 @@ public class MeleeAttacker : AbilityBase, MeleeAttack.ISubscriber
 
     private void StartNextCombo()
     {
-        if (comboCount + 1 < combos.Count)
+        if (comboCount + 1 < currentCombos.Count)
         {
             if (comboResetTimer != null)
                 StopCoroutine(comboResetTimer);
 
             comboCount++;
-            ComboInformation currentCombo = combos[comboCount];
+            ComboInformation currentCombo = currentCombos[comboCount];
 
             abilityHolder.OccupyMovement(true);
             isOccupiedByThis = true;
@@ -108,13 +129,10 @@ public class MeleeAttacker : AbilityBase, MeleeAttack.ISubscriber
             comboResetTimer = StartCoroutine(ResetComboCount(currentCombo.duration));
 
             foreach (DataOnTime<Vector2> velocityOnTime in currentCombo.velocityOnTimes)
-                StartCoroutine(Job(() => WaitForSecondsRoutine(velocityOnTime.time), () => AddVelocity(velocityOnTime.data)));
+                StartCoroutine(Job(() => WaitForSecondsRoutine(velocityOnTime.time), () => AddVelocity(velocityOnTime.data) ));
 
             foreach (DataOnTime<MeleeAttack> attackOnTime in currentCombo.attackOnTime)
-                StartCoroutine(Job(() => WaitForSecondsRoutine(attackOnTime.time), () =>
-                {
-                    attackOnTime.data.Activate();
-                }));
+                StartCoroutine(Job(() => WaitForSecondsRoutine(attackOnTime.time), () =>  attackOnTime.data.Activate() ));
 
             abilityHolder.ModelAnimator.SetInteger(comboCountStringToHash, comboCount);
         }
@@ -123,13 +141,14 @@ public class MeleeAttacker : AbilityBase, MeleeAttack.ISubscriber
     private IEnumerator ResetComboCount(float time)
     {
         yield return new WaitForSeconds(time);
-        isInitializing = true;
-        StartCoroutine(Job(() => WaitForSecondsRoutine(0.25f), () => isInitializing = false));
+        isResetting = true;
+        StartCoroutine(Job(() => WaitForSecondsRoutine(0.25f), () => isResetting = false));
         comboCount = -1;
         abilityHolder.ModelAnimator.SetInteger(comboCountStringToHash, comboCount);
         abilityHolder.OccupyMovement(false);
         isOccupiedByThis = false;
         comboResetTimer = null;
+        currentCombos = null;
     }
 
     private void AddVelocity(Vector2 velocity)
